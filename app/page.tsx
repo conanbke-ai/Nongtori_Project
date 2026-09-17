0e7ad31049a8ad3bb2bb3ecd924ff8faae0f0895
conanbke-ai <conanbke@gmail.com>
2026-09-17 15:44:14 +0900

    feat: render notification bridge with farmer dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 import { FarmerDashboard } from './components/FarmerDashboard';
+import { NotificationBridge } from './features/notifications/presentation/NotificationBridge';
 
 export default function Home() {
-  return <FarmerDashboard />;
+  return <>
+    <FarmerDashboard />
+    <NotificationBridge />
+  </>;
 }
